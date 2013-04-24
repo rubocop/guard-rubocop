6c6c9099b8a69e05c95d74c834ae0d44fc171ec2
Yuji Nakayama <nkymyj@gmail.com>
2013-04-24 23:08:50 +0900

    Use Coveralls as a SimpleCov formatter rather than standalone

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,22 +8,20 @@ Dir[File.join(File.dirname(__FILE__), 'support', '*')].each do |path|
   require path
 end
 
+require 'simplecov'
+SimpleCov.coverage_dir(File.join('spec', 'coverage'))
+
 if ENV['TRAVIS']
   require 'coveralls'
-  Coveralls.wear!
-else
-  require 'simplecov'
-  SimpleCov.coverage_dir(File.join('spec', 'coverage'))
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+elsif ENV['CI']
+  require 'simplecov-rcov'
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+end
 
-  if ENV['CI']
-    require 'simplecov-rcov'
-    SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-  end
-
-  SimpleCov.start do
-    add_filter '/spec/'
-    add_filter '/vendor/bundle/'
-  end
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/vendor/bundle/'
 end
 
 require 'guard/rubocop'
