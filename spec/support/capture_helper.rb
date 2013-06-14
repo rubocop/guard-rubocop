37967bafd8d809d6949e4cec30f971f543da30ed
Yuji Nakayama <nkymyj@gmail.com>
2013-06-14 22:48:23 +0900

    Revert "Let Travis CI run rubocop"
    This reverts commit fd2ffa5b404dfd52b21b0c1046eadc242f6d3f10.
    RuboCop 0.8.2 has a bug which Syntax cop cannot be disabled.

--- spec/support/capture_helper.rb
+++ spec/support/capture_helper.rb
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 module CaptureHelper
-  # rubocop:disable Eval
   def capture(stream_name)
     stream_name = stream_name.to_s.downcase
     original_stream = eval("$#{stream_name}")
@@ -16,5 +15,4 @@ module CaptureHelper
 
     result
   end
-  # rubocop:enable Eval
 end
