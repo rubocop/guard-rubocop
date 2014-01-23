daf0e9e9df7d6932bf1cee8543347317822200c8
Yuji Nakayama <nkymyj@gmail.com>
2014-01-24 01:34:29 +0900

    Enforce expect syntax with rspec-mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,10 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.mock_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
 
 Dir[File.join(File.dirname(__FILE__), 'support', '*')].each do |path|
