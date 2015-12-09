206c8a220307fa594e6a0542285a43293fcc985b
Yuji Nakayama <nkymyj@gmail.com>
2015-12-09 20:32:51 +0900

    Improve RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,21 @@
 # coding: utf-8
 
 RSpec.configure do |config|
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  config.mock_with :rspec do |c|
-    c.syntax = :expect
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
   end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  config.order = :random
+  Kernel.srand config.seed
 end
 
 Dir[File.join(File.dirname(__FILE__), 'support', '*')].each do |path|
